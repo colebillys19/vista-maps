365ff5f2a21aa1e3ab65fe32d75115279dfcaaa7
Billy Cole <cole.billys@gmail.com>
2020-04-10 09:42:54 -0700

    mock data

--- app/components/VistaList/mockData.ts
+++ app/components/VistaList/mockData.ts
@@ -1,21 +1,21 @@
 const mockData = [
   {
-    author: "Billy",
-    imageUrl: "https://res.cloudinary.com/dxklaorw6/image/upload/v1542642062/lktqn9nyu8vbnr93vyei.jpg",
-    name: "Los Angeles",
-    rating: "5",
+    author: 'Billy',
+    imageUrl: 'https://res.cloudinary.com/dxklaorw6/image/upload/v1542642062/lktqn9nyu8vbnr93vyei.jpg',
+    name: 'Los Angeles',
+    rating: '5',
   },
   {
-    author: "Dave",
-    imageUrl: "https://res.cloudinary.com/dxklaorw6/image/upload/v1542640644/btthnnhdsaivzvpmze14.jpg",
-    name: "Hamden",
-    rating: "5",
+    author: 'Dave',
+    imageUrl: 'https://res.cloudinary.com/dxklaorw6/image/upload/v1542640644/btthnnhdsaivzvpmze14.jpg',
+    name: 'Hamden',
+    rating: '5',
   },
   {
-    author: "Barry",
-    imageUrl: "https://res.cloudinary.com/dxklaorw6/image/upload/v1542631086/cwctck4kq7sfbnqpeqbh.jpg",
-    name: "Norwalk",
-    rating: "5",
+    author: 'Barry',
+    imageUrl: 'https://res.cloudinary.com/dxklaorw6/image/upload/v1542631086/cwctck4kq7sfbnqpeqbh.jpg',
+    name: 'Norwalk',
+    rating: '5',
   },
 ];
 
